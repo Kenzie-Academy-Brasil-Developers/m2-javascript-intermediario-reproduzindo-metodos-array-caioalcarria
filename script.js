65243f4662c59b93d5f61302886d4224837f0a8f
caio <alcarriacaio@gmail.com>
2022-07-25 19:44:48 -0300

    final

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-// Método Map ---------
+// Método map
 
 const arrayMap = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
 
 // função callback
 // Ela apenas imprime o elemento na posição do index e de que array ele veio
 function callbackMap(element, index, array) {
-  return `Número ${element} no index: ${index}, veio desse array: ${array}`;
+  
+  return `Número ${element} na posição: ${index}, veio desse array: ${array}`;
 }
 
 function map(array, callback) {
-  //sua lógica
+  //sua lógica aqui
+  result = []
+  for (let i=0; i<array.length; i++){
+    result.push(callback(array[i], i, array))
+  }
+  return result
 }
 
-//console.table(map(arrayMap, callbackMap));
+console.table(map(arrayMap, callbackMap));
 
-//Método Filter ---------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*Método Filter ---------
 
 const arrayFilter = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
 
@@ -22,17 +44,41 @@ const arrayFilter = [1, 2, 3, 4, 5]; //esse é o array que você terá que itera
 // Ela apenas imprime o elemento que for maior que 2
 function callbackFilter(element, index, array) {
   if (element > 2 && index && array.length > 2) {
-    return true;
+    return element;
+    
   }
 }
 
 function filter(array, callback) {
   //sua lógica
+  result = []
+  for (let i=0; i<array.length; i++){
+    
+    if(callback(array[i], i, array) != undefined){
+        result.push(callback(array[i], i, array))
+    }
+    
+  }
+  return result
 }
 
-//console.log(filter(arrayFilter, callbackFilter));
+console.log(filter(arrayFilter, callbackFilter));
 
-//Método Reduce ---------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*Método Reduce ---------
 
 const arrayReduce = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
 
@@ -44,7 +90,12 @@ function callbackReduce(acumulator, valorAtual) {
 
 function reduce(array, callback, initialValue = 0) {
   //sua lógica
+  for (let i=0; i<array.length; i++){
+    initialValue = callback(initialValue, array[i])
+    
+  }
+  return initialValue
 }
 
-// console.log(reduce(arrayReduce, callbackReduce));
-// console.log(reduce(arrayReduce, callbackReduce, 50));
+ console.log(reduce(arrayReduce, callbackReduce));
+ console.log(reduce(arrayReduce, callbackReduce, 50));
